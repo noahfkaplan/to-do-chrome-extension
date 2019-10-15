0974a9c57ef3a92949dec080d6850d242c86e053
Noah Kaplan <nkaplan1@uncc.edu>
2019-10-14 20:25:21 -0400

    updated with materialUI components

--- src/ListItem/ListItem.js
+++ src/ListItem/ListItem.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
 
-const ListItem = (props) =>{
+const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    input: {
+      margin: theme.spacing(1),
+    },
+  }));
+
+const ListItem = () =>{
+    const classes = useStyles();
+    const [checked, setChecked] = useState(false);
+    const [itemText, setItemText] = useState("");    
+    const handleCheckChange = event => {
+        setChecked( event.target.checked );
+    };
+    const handleItemTextChange = event => {
+        setItemText( event.target.value );
+    }    
     return(
         <div>
-            <input type = 'checkbox'/>
-            <input type = 'text' value = {props.children}/>
+            <Checkbox
+                checked={checked}
+                onChange={handleCheckChange}
+                value="checked"
+                inputProps={{
+                    'aria-label': 'primary checkbox',
+                }}
+            />
+            <Input
+                placeholder={"Placeholder Text"}
+                value={itemText}
+                onChange={handleItemTextChange}
+                className={classes.input}
+                inputProps={{
+                    'aria-label': 'description',
+                }}
+            />
         </div>
     );
 }
