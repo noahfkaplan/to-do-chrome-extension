14990a43f510d148ecc7084f48848f13e0462a3f
Noah Kaplan <nkaplan1@uncc.edu>
2019-10-16 21:21:49 -0400

    fixed state issue

--- src/ListArea/ListArea.js
+++ src/ListArea/ListArea.js
@@ -2,24 +2,27 @@ import React, { useState } from 'react';
 import ListItem from '../ListItem'
 
 const ListArea = () => {
-    const [list, setList] = useState([{ text: "default item", checked: false },{ text: "default item", checked: false },{ text: "default item", checked: false }]);
+    const [list, setList] = useState([{ id: 0, text: "default item", checked: false }]);
     
-    const handleCheckChange = text => event => {
-        let updatedList = list.map(item => item.text === text ? {...item, checked: event.target.checked} : item );
-        setList(updatedList);
+    const handleCheckChange = id => event => {
+        setList(
+            list.map(item => item.id === id ? {...item, checked: event.target.checked} : item )
+        );
     };
-    const handleTextChange = text => event => {
-        let updatedList = list.map(item => item.text === text ? {...item, text: event.target.value}: item );
-        setList(updatedList);
+    
+    const handleTextChange = id => event => {
+        setList(
+            list.map(item => item.id === id ? {...item, text: event.target.value}: item )
+        );
     }
 
     return(
         <div className = 'mainArea'>
             {list.map(item => {
                 return <ListItem
-                    key = {item.text} 
-                    onCheckChange = {() => handleCheckChange(item.text)} 
-                    onTextChange = {() => handleTextChange(item.text)} 
+                    key = {item.id} 
+                    onCheckChange = {() => handleCheckChange(item.id)} 
+                    onTextChange = {() => handleTextChange(item.id)} 
                     text = {item.text} 
                     checked = {item.checked}>
                 </ListItem>
