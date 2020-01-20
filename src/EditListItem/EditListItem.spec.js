56c04d0f1f85af4269f6e20aafea726253c1cdb6
Noah Kaplan <nkaplan1@uncc.edu>
2020-01-20 14:19:53 -0500

    added tests for new delete functionality

--- src/EditListItem/EditListItem.spec.js
+++ src/EditListItem/EditListItem.spec.js
@@ -2,12 +2,6 @@ import React from 'react';
 import { render, cleanup, wait, fireEvent } from "@testing-library/react";
 import EditListItem from './EditListItem';
 
-const defaultItem = {
-    id: -1, 
-    text: "",
-    checked: false,
-    url: "",
-}
 const testItem = {
     id: 0,
     text: 'Default Text', 
@@ -15,8 +9,8 @@ const testItem = {
     url: 'http://www.google.com'
 }
 
-const renderEditListItem = (onSave, item) => {
-    return render(<EditListItem onSave = {onSave} item = {item}/>);
+const renderEditListItem = (onSave, onDelete, item) => {
+    return render(<EditListItem onSave = {onSave} onDelete = {onDelete} item = {item}/>);
 };
 
 describe("Edit List Item", () => {
@@ -24,38 +18,58 @@ describe("Edit List Item", () => {
     afterEach(cleanup)
 
     it("renders without crashing", () => {
-        renderEditListItem(jest.fn(), testItem);
+        renderEditListItem(jest.fn(), jest.fn(), testItem);
     });
 
     it("fills description with editted item's text", () => {
-        const { getByLabelText, debug } = renderEditListItem(jest.fn(), testItem);
+        const { getByLabelText, debug } = renderEditListItem(jest.fn(), jest.fn(), testItem);
         const description = getByLabelText("editDescription");
         expect(description.querySelector("input").value).toBe(testItem.text);
     });
 
     it("fills url with editted item's url", () => {
-        const { getByLabelText, debug } = renderEditListItem(jest.fn(), testItem);
+        const { getByLabelText, debug } = renderEditListItem(jest.fn(), jest.fn(), testItem);
         const url = getByLabelText("editUrl");
         expect(url.querySelector("input").value).toBe(testItem.url);
     });
 
     it("renders description with no value when null item passed in", () => {
-        const { getByLabelText, debug } = renderEditListItem(jest.fn(), defaultItem);
+        const { getByLabelText, debug } = renderEditListItem(jest.fn(), jest.fn(), null);
         const description = getByLabelText("editDescription");
-        expect(description.querySelector("input").value).toBe(defaultItem.text);
+        expect(description.querySelector("input").value).toBe('');
     });
 
     it("renders url with no value when null item passed in", () => {
-        const { getByLabelText, debug } = renderEditListItem(jest.fn(), defaultItem);
+        const { getByLabelText, debug } = renderEditListItem(jest.fn(), jest.fn(), null);
         const url = getByLabelText("editUrl");
-        expect(url.querySelector("input").value).toBe(defaultItem.url);
+        expect(url.querySelector("input").value).toBe('');
     })
+
+    it("renders save button and delete button when editting existing user", () => {
+        const { getByText } = renderEditListItem(jest.fn(), jest.fn(), testItem);
+        getByText("Save") //implicit assertion
+        getByText("Delete") //implicit assertion
+    });
+    
+    it("renders save button and not delete button when creating new user", () => {
+        const { getByText, queryByText } = renderEditListItem(jest.fn(), jest.fn(), null)
+        getByText("Save") //implicit assertion
+        expect(queryByText("Delete")).toBeFalsy();
+    })
+
+    it("calls onDelete when Delete button is clicked", () => {
+        const onDelete = jest.fn();
+        const { getByText } = renderEditListItem(jest.fn(), onDelete, testItem);
+        const deleteButton = getByText("Delete");
+        fireEvent.click(deleteButton);
+        expect(onDelete).toBeCalled();
+    });
 
     it("calls onSave with description and url when save button is clicked", () => {
         const onSave = jest.fn()
-        const { getByText } = renderEditListItem(onSave, testItem);
+        const { getByText } = renderEditListItem(onSave, jest.fn(), testItem);
         const saveButton = getByText("Save");
-        fireEvent.click(saveButton)
+        fireEvent.click(saveButton);
         expect(onSave).toBeCalledWith(testItem.text, testItem.url);
     });
 });
